1ca26182944453ef682097e9ae042b67fae7f5da
Alexandre Lavoie <alexandre.lavoie00@gmail.com>
2021-11-01 03:13:25 -0400

    Added scoreboard logging.

--- line_em_up/log/__init__.py
+++ line_em_up/log/__init__.py
@@ -1,5 +1,5 @@
 from ..common.sql import Game
-from ..common import PlayerType, AlgorithmType, Tile
+from ..common import PlayerType, AlgorithmType, Tile, HeuristicType
 from .config import LogConfig
 
 from sqlalchemy import create_engine
@@ -28,10 +28,9 @@ def board_str(tiles, blocks, size):
     return '\n'.join(board_lines)
 
 def make_game_traces(db_session: any, log_dir: str):
-    for game in db_session.query(Game).all():
-        if not game.complete: 
-            continue
+    games = [game for game in db_session.query(Game).all() if game.complete]
 
+    for game in games:
         log = []
 
         log.append(f"id={game.id}")
@@ -81,7 +80,7 @@ def make_game_traces(db_session: any, log_dir: str):
         if len(all_statistics) > 0:
             log.append(f"6(b)i   Average evaluation time: {sum(statistics.average_time for statistics in all_statistics) / len(all_statistics)}s")
             log.append(f"6(b)ii  Total heuristic evaluations: {sum(sum(statistics.depth_counts) for statistics in all_statistics)}")
-            log.append(f"6(b)iii Evaluations by depths: {[sum(depths) for depths in itertools.zip_longest(*[statistics.depth_counts for statistics in all_statistics])]}")
+            log.append(f"6(b)iii Evaluations by depths: {[sum(dv) for dv in itertools.zip_longest(*[statistics.depth_counts for statistics in all_statistics], fillvalue=0)]}")
             log.append(f"6(b)iv  Average evaluation depth: {sum(statistics.average_depth for statistics in all_statistics) / len(all_statistics)}")
             log.append(f"6(b)v   Average recursion depth: {sum(statistics.average_recursive_depth for statistics in all_statistics) / len(all_statistics)}")
             log.append(f"6(b)vi  Total moves: {len(game.moves)}")
@@ -91,14 +90,79 @@ def make_game_traces(db_session: any, log_dir: str):
 
     db_session.close()
 
+def make_scoreboard(db_session: any, log_dir: str):
+    games = [game for game in db_session.query(Game).all() if game.complete]
+
+    log = []
+
+    log.append(f"n={games[0].board_size} b={games[0].block_count} s={games[0].line_up_size} t={games[0].max_time}")
+    log.append("")
+
+    for session in games[0].unique_sessions:
+        log.append(f"Player {session.player.name}: d={session.depth} a={session.algorithm == AlgorithmType.ALPHABETA} e{session.heuristic}")
+
+    log.append("")
+    log.append(f"{len(games)} games")
+    log.append("")
+
+    for heuristic in HeuristicType:
+        win_count = sum(len([session for session in game.unique_sessions if session.win and session.heuristic == heuristic]) for game in games)
+        log.append(f"Total wins for heuristic e{heuristic.value}: {win_count} ({(win_count / len(games)) * 100}%)")
+
+    log.append("")
+
+    average_times = 0
+    evaluation_count = 0
+    depth_counts = []
+    average_depth = 0
+    average_recursive_depth = 0
+    average_moves = 0
+
+    stats_count = 0
+    for game in games:
+        all_statistics = [move.statistics[0] for move in game.move_tiles if move.statistics]
+
+        if len(all_statistics) == 0:
+            continue
+
+        stats_count += 1
+        average_times += sum(statistics.average_time for statistics in all_statistics) / len(all_statistics)
+        evaluation_count += sum(sum(statistics.depth_counts) for statistics in all_statistics)
+        game_depth_counts = [sum(dv) for dv in itertools.zip_longest(*[statistics.depth_counts for statistics in all_statistics], fillvalue=0)]
+        depth_counts = [sum(dv) for dv in itertools.zip_longest(depth_counts, game_depth_counts, fillvalue=0)]
+        average_depth += sum(statistics.average_depth for statistics in all_statistics) / len(all_statistics)
+        average_recursive_depth += sum(statistics.average_recursive_depth for statistics in all_statistics) / len(all_statistics)
+        average_moves += len(game.moves)
+
+    average_times /= stats_count
+    average_depth /= stats_count
+    average_recursive_depth /= stats_count
+    average_moves /= stats_count
+
+    log.append(f"i   Average evaluation time: {average_times}s")
+    log.append(f"ii  Total heuristic evaluations: {evaluation_count}")
+    log.append(f"iii Evaluations by depth: {depth_counts}")
+    log.append(f"iv  Average evaluation depth: {average_depth}")
+    log.append(f"v   Average recursion depth: {average_recursive_depth}")
+    log.append(f"vi  Average moves per game: {average_moves}")
+
+    with open(os.path.join(log_dir, f"scoreboard.txt"), "w") as h:
+        h.write('\n'.join(log))
+
+    db_session.close()
+
 def log_main(config: LogConfig):
     engine = create_engine(
-        f'sqlite:///{config.db}',
-        connect_args={"check_same_thread": False},
+        f'sqlite:///{config.db}'
     )
     SessionMaker = sessionmaker(bind=engine)
 
     make_game_traces(
+        db_session=SessionMaker(),
+        log_dir="./logs/"
+    )
+
+    make_scoreboard(
         db_session=SessionMaker(),
         log_dir="./logs/"
     )
